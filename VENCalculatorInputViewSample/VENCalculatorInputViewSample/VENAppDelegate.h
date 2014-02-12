7219f4785b772e461dd4088332bdc9c01df86cab
Ayaka Nonaka <ayanonagon@gmail.com>
2014-02-12 13:21:43 -0800

    Clean up sample project.

--- VENCalculatorInputViewSample/VENCalculatorInputViewSample/VENAppDelegate.h
+++ VENCalculatorInputViewSample/VENCalculatorInputViewSample/VENAppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  VENAppDelegate.h
-//  VENCalculatorInputViewSample
-//
-//  Created by Ayaka Nonaka on 2/11/14.
-//  Copyright (c) 2014 Venmo. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface VENAppDelegate : UIResponder <UIApplicationDelegate>
--- VENCalculatorInputViewSample/VENCalculatorInputViewSample/VENViewController.h
+++ VENCalculatorInputViewSample/VENCalculatorInputViewSample/VENViewController.h
@@ -1,11 +1,3 @@
-//
-//  VENViewController.h
-//  VENCalculatorInputViewSample
-//
-//  Created by Ayaka Nonaka on 2/11/14.
-//  Copyright (c) 2014 Venmo. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface VENViewController : UIViewController
