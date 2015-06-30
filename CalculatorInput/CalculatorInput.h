137d0d4bb47fe604984d2db227768624e266f55d
Sam Soffes <sam@soff.es>
2015-06-29 18:13:04 -0700

    Fix umbrella header

--- CalculatorInput/CalculatorInput.h
+++ CalculatorInput/CalculatorInput.h
@@ -6,8 +6,8 @@ FOUNDATION_EXPORT double CalculatorInputVersionNumber;
 //! Project version string for CalculatorInputView.
 FOUNDATION_EXPORT const unsigned char CalculatorInputVersionString[];
 
-#import <CalculatorInputView/CalculatorInputView.h>
-#import <CalculatorInputView/CalculatorInputTextField.h>
-#import <CalculatorInputView/MoneyCalculator.h>
-#import <CalculatorInputView/NSString+CalculatorInputView.h>
-#import <CalculatorInputView/UITextField+CalculatorInputView.h>
+#import <CalculatorInput/CalculatorInputView.h>
+#import <CalculatorInput/CalculatorInputTextField.h>
+#import <CalculatorInput/MoneyCalculator.h>
+#import <CalculatorInput/NSString+CalculatorInputView.h>
+#import <CalculatorInput/UITextField+CalculatorInputView.h>
