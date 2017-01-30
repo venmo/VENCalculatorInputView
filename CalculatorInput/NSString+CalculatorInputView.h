d97c98861322f5c6086a9bf9c58aca2aad524053
Mark Adams <mark.adams86@gmail.com>
2017-01-30 18:18:03 -0500

    Clear evaluated text when only remaining character is the prefix
    * Audits CalculatorInputTextField for nullability.
    * Adds an extra call to `deleteBackwards` if the only remaining text is
    the prefix character.

--- CalculatorInput/NSString+CalculatorInputView.h
+++ CalculatorInput/NSString+CalculatorInputView.h
@@ -1,8 +1,12 @@
 @import Foundation;
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface NSString (CalculatorInputView)
 
 - (NSString *)ven_stringByReplacingCharactersInSet:(NSCharacterSet *)characterSet
                                     withString:(NSString *)string;
 
 @end
+
+NS_ASSUME_NONNULL_END
