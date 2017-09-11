f7b3499df9a6b913c4f76c7afd1d34fff5ac413d
Daniel Thompson <daniel@venmo.com>
2017-09-11 16:49:18 -0400

    Update tests to Swift 3 and fix configuration error
    Some of the tests used syntax used prior to Swift 3 and that was updated
    to the current syntax. In addition, the Product Name of the test target
    was changed to be CalculatorInput, which had the side effect of making
    imports not work correctly. When set back to the previous value of
    CalculatorInputTests, the tests are able to find the referenced types
    and the tests pass.
    
    The TravisCI configuration is still not correct, so I expect that to
    fail.

--- Tests/StringExtensionTests.swift
+++ Tests/StringExtensionTests.swift
@@ -4,13 +4,13 @@ import CalculatorInput
 
 class StringExtensionTests: XCTestCase {
     func testWhitespace() {
-        let result = NSString(string: "foo     bar\n           ").ven_stringByReplacingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet(), withString: "")
+        let result = NSString(string: "foo     bar\n           ").ven_stringByReplacingCharacters(in: CharacterSet.whitespacesAndNewlines, with: "")
         XCTAssertEqual("foobar", result)
     }
 
     func testCustomCharacterSet() {
-        let characterSet = NSCharacterSet(charactersInString: "0123456789-/*.+").invertedSet
-        let result = NSString(string: "1+1(&^!@#+1foobar").ven_stringByReplacingCharactersInSet(characterSet, withString: "")
+        let characterSet = CharacterSet(charactersIn: "0123456789-/*.+").inverted
+        let result = NSString(string: "1+1(&^!@#+1foobar").ven_stringByReplacingCharacters(in: characterSet, with: "")
         XCTAssertEqual("1+1+1", result)
     }
 }
